337d2497ae4941d2fb24b722ad6549eff50c8857
curability4apish <89716925+curability4apish@users.noreply.github.com>
2025-01-22 22:22:19 +0800

    Update cfgAdapter.js

--- cfgAdapter.js
+++ cfgAdapter.js
@@ -1,64 +1,17 @@
 var CfgAdapter = {};
 
 CfgAdapter.load = function(siteKey) {
-	let result = this.v1.load(siteKey);
-	if(result) return result;
-	
-	result = this.v0.load(siteKey);
-	if(result) {
-		this.v1.save(siteKey, result);
-		this.v0.remove(siteKey);
-		return result;
-	}
-	
-	return {
-		size:16,
-		charsetFlag:0xf,
-	};
+    // Removed v1 and v0 calls as they used localStorage
+    return {
+        size: 16,
+        charsetFlag: 0xf,
+    };
 };
+
 CfgAdapter.save = function(siteKey, cfg) {
-	this.v1.save(siteKey, cfg);
+    // Removed v1 save call as it used localStorage
 };
 
+// Removed CfgAdapter.v0 as it used localStorage
 
-CfgAdapter.v0 = {};
-
-CfgAdapter.v0.getStgKey = function(siteKey) {
-	return 'pw.site.' + wrapper.wrap16(sha1.digest(siteKey));
-};
-CfgAdapter.v0.load = function(siteKey) {
-	let result = localStorage.getItem(this.getStgKey(siteKey));
-	if(!result) return null;
-	
-	result = JSON.parse(result);
-	let flag = result.charsNumberChecked
-		| (result.charsLowercaseChecked << 1)
-		| (result.charsUppercaseChecked << 2)
-		| (result.charsSymbolChecked << 3)
-	;
-	result = {
-		size:result.size,
-		charsetFlag:flag,
-		isForceAllSet:0,
-	};
-	return result;
-};
-CfgAdapter.v0.remove = function(siteKey) {
-	localStorage.removeItem(this.getStgKey(siteKey));
-};
-
-
-CfgAdapter.v1 = {};
-
-CfgAdapter.v1.getStgKey = function(siteKey) {
-	return 'siteCfg.' + wrapper.wrap64(sha1.digest(siteKey));
-};
-CfgAdapter.v1.load = function(siteKey) {
-	return JSON.parse(localStorage.getItem(this.getStgKey(siteKey)));
-};
-CfgAdapter.v1.save = function(siteKey, cfg) {
-	localStorage.setItem(this.getStgKey(siteKey), JSON.stringify(cfg));
-};
-CfgAdapter.v1.remove = function(siteKey) {
-	localStorage.removeItem(this.getStgKey(siteKey));
-};
+// Removed CfgAdapter.v1 as it used localStorage
